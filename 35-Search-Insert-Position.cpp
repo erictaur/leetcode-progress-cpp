ad5a3bbe4184a50d50cea361a7174a51202ed55f
Ke Haur Taur <erictaur@outlook.com>
2021-02-02 01:46:52 -0500

    Update and rename Search Insert Position.cpp to 35-Search-Insert-Position.cpp

--- 35-Search-Insert-Position.cpp
+++ 35-Search-Insert-Position.cpp
@@ -18,3 +18,15 @@ public:
         return ans;
     }
 };
+
+/* 35. Search-Insert-Position.cpp
+//////////////////////////////////////////////////
+Given a sorted array of distinct integers and a target value, return the index if the target is found. 
+If not, return the index where it would be if it were inserted in order.
+
+Input: nums = [1,3,5,6], target = 5
+Output: 2
+
+https://leetcode.com/problems/search-insert-position/
+//////////////////////////////////////////////////
+*/
