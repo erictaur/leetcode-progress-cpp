bd79c33d21ffea0d0aafb20489d5e4f4cb7cbc5a
Ke Haur Taur <erictaur@outlook.com>
2021-02-02 01:44:40 -0500

    Update 27-Remove-Element.cpp

--- 27-Remove-Element.cpp
+++ 27-Remove-Element.cpp
@@ -21,6 +21,12 @@ Given an array nums and a value val, remove all instances of that value in-place
 Do not allocate extra space for another array, you must do this by modifying the input array in-place with O(1) extra memory.
 The order of elements can be changed. It doesn't matter what you leave beyond the new length.
 
+Input: nums = [3,2,2,3], val = 3
+Output: 2, nums = [2,2]
+Explanation: Your function should return length = 2, with the first two elements of nums being 2.
+It doesn't matter what you leave beyond the returned length. 
+For example if you return 2 with nums = [2,2,3,3] or nums = [2,2,0,0], your answer will be accepted.
+
 https://leetcode.com/problems/remove-element/
 //////////////////////////////////////////////////
 */
