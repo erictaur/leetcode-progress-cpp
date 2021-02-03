d6918547755ab8c18cf94ba312e6557c3d0474b9
Ke Haur Taur <erictaur@outlook.com>
2021-02-03 01:17:54 -0500

    Create 66-Plus-One.cpp

---
+++ 66-Plus-One.cpp
@@ -0,0 +1,34 @@
+class Solution {
+public:
+    vector<int> plusOne(vector<int>& digits) {
+        for(int i = digits.size()-1; i >= 0; --i){
+            if (digits[i] < 9){
+                digits[i]++;
+                return digits;
+            }
+            else{
+                digits[i] = 0;
+            }
+        }
+        digits[0] = 1;
+        digits.push_back(0);
+        return digits;
+    }
+};
+
+/* 66. Plus-One.cpp
+//////////////////////////////////////////////////
+Given a non-empty array of decimal digits representing a non-negative integer, increment one to the integer.
+
+The digits are stored such that the most significant digit is at the head of the list, and each element in the array contains a single digit.
+
+You may assume the integer does not contain any leading zero, except the number 0 itself.
+
+Input: digits = [1,2,3]
+Output: [1,2,4]
+Explanation: The array represents the integer 123.
+
+temp += digits[(digits.size()-1)-i]*pow(10.0, i);
+https://leetcode.com/problems/plus-one/
+//////////////////////////////////////////////////
+*/
