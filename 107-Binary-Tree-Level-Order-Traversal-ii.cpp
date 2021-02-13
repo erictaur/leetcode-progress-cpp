bb2c83f65e0808cf85c8f80e2ada590b4e3ac13c
Ke Haur Taur <erictaur@outlook.com>
2021-02-13 03:15:00 -0500

    Create 107-Binary-Tree-Level-Order-Traversal-ii.cpp

---
+++ 107-Binary-Tree-Level-Order-Traversal-ii.cpp
@@ -0,0 +1,49 @@
+class Solution {
+public:
+    vector<vector<int>> levelOrderBottom(TreeNode* root) {
+        vector<vector<int>> traverse;
+        queue<TreeNode*> q;
+        
+        if(root == nullptr) return traverse;
+        
+        q.push(root);
+        while(!q.empty()){
+            int numToVisit = q.size();
+            vector<int> curLevel;
+            
+            for(int i = 0; i < numToVisit; i++){
+                TreeNode* atNode = q.front();
+                curLevel.push_back(atNode->val);
+                q.pop();
+                
+                if(atNode->left != nullptr) q.push(atNode->left);
+                if(atNode->right != nullptr) q.push(atNode->right);
+            }
+            traverse.push_back(curLevel);
+        }
+        reverse(traverse.begin(), traverse.end());
+        
+        return traverse;
+    }
+};
+
+/* 107. Binary-Tree-Level-Order-Traversal.cpp
+//////////////////////////////////////////////////
+Given a binary tree, return the level order traversal of its nodes' values. (ie, from left to right, level by level).
+For example:
+Given binary tree [3,9,20,null,null,15,7],
+    3
+   / \
+  9  20
+    /  \
+   15   7
+return its bottom-up level order traversal as:
+[
+  [15,7],
+  [9,20],
+  [3]
+]
+
+https://leetcode.com/problems/binary-tree-level-order-traversal-ii/
+//////////////////////////////////////////////////
+*/
