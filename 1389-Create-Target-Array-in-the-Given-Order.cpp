132a5eade035b01e21c1cf086e36f235c64f1f27
Ke Haur Taur <erictaur@outlook.com>
2021-03-26 12:47:52 -0400

    Create 1389-Create-Target-Array-in-the-Given-Order.cpp

---
+++ 1389-Create-Target-Array-in-the-Given-Order.cpp
@@ -0,0 +1,45 @@
+class Solution {
+public:
+    vector<int> createTargetArray(vector<int>& nums, vector<int>& index) {
+        
+        vector<int> ans(nums.size(), 0);
+        
+        for(int i=0; i<nums.size(); i++){
+            int sweep = nums.size()-1;
+            ans[sweep] = nums[i];
+
+            while(sweep != index[i]){
+                swap(ans[sweep], ans[--sweep]);
+            }
+        }
+        
+        return ans;
+    }
+};
+
+/* 1389. Create-Target-Array-in-the-Given-Order.cpp
+//////////////////////////////////////////////////
+Given two arrays of integers nums and index. Your task is to create target array under the following rules:
+
+Initially target array is empty.
+From left to right read nums[i] and index[i], insert at index index[i] the value nums[i] in target array.
+Repeat the previous step until there are no elements to read in nums and index.
+Return the target array.
+
+It is guaranteed that the insertion operations will be valid.
+
+Example 1:
+
+Input: nums = [0,1,2,3,4], index = [0,1,2,2,1]
+Output: [0,4,1,3,2]
+Explanation:
+nums       index     target
+0            0        [0]
+1            1        [0,1]
+2            2        [0,1,2]
+3            2        [0,1,3,2]
+4            1        [0,4,1,3,2]
+
+https://leetcode.com/problems/create-target-array-in-the-given-order/
+//////////////////////////////////////////////////
+*/
